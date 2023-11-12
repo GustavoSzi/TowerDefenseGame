b07e73173e94fa7dcb7d7335c3d4530e77aaa58a
Gustavo <gustavoszilagyi3@gmail.com>
2023-11-12 18:38:14 -0300

    :sparkles: FEAT: Basic setup for selecting placeable tiles
    - New events used: OnMouseOver, OnMouseDown and OnMouseExit;
    - Overlay for the targeted tile

--- Assets/Scripts/Tiles/Waypoint.cs
+++ Assets/Scripts/Tiles/Waypoint.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private Overlay overlay;
+
+    [SerializeField]
+    private bool isPlaceable = true;
+
+    private void OnMouseOver()
     {
-        
+        if (!isPlaceable) return;
+            
+        overlay.gameObject.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseExit()
     {
-        
+        if (!isPlaceable) return;
+
+        overlay.gameObject.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        if (!isPlaceable) return;
+
+        Debug.Log(transform.name);
     }
 }
